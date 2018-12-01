56ded027912cdeb20486338962034e281ad8f145
Thomas <thomas@craftacademy.se>
2018-12-01 17:56:35 +0100

    basic react configuration

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {hot} from "react-hot-loader";
+
+
+const App = () => {
+    return <div>React Boilerplate</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
